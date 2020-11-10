3caa2a9ae341f8f207b5dc0f4f09d48b42d7bdeb
Djordje Mijovic <djordje.mijovic@gmail.com>
2020-11-10 17:41:52 +0100

    [Sol->Yul] Implementing copying of calldata arrays to memory

--- libsolidity/codegen/YulUtilFunctions.cpp
+++ libsolidity/codegen/YulUtilFunctions.cpp
@@ -2953,7 +2953,8 @@ string YulUtilFunctions::conversionFunction(Type const& _from, Type const& _to)
 
 string YulUtilFunctions::arrayConversionFunction(ArrayType const& _from, ArrayType const& _to)
 {
-	solUnimplementedAssert(_to.location() != DataLocation::CallData, "Conversion of calldata types not yet implemented.");
+	solAssert(_to.location() != DataLocation::CallData, "");
+
 	// Other cases are done explicitly in LValue::storeValue, and only possible by assignment.
 	if (_to.location() == DataLocation::Storage)
 		solAssert(
@@ -2962,11 +2963,7 @@ string YulUtilFunctions::arrayConversionFunction(ArrayType const& _from, ArrayTy
 			"Invalid conversion to storage type."
 		);
 	if (_to.location() == DataLocation::Memory && _from.location() == DataLocation::CallData)
-	{
-		solUnimplementedAssert(_from.isDynamicallySized(), "");
-		solUnimplementedAssert(!_from.baseType()->isDynamicallyEncoded(), "");
-		solUnimplementedAssert(_from.isByteArray() && _to.isByteArray() && _to.isDynamicallySized(), "");
-	}
+		solUnimplementedAssert(!_from.baseType()->isDynamicallySized(), "");
 
 	string functionName =
 		"convert_array_" +
@@ -2994,18 +2991,18 @@ string YulUtilFunctions::arrayConversionFunction(ArrayType const& _from, ArrayTy
 				"body",
 				Whiskers(R"(
 					// Copy the array to a free position in memory
+					converted :=
 					<?fromStorage>
-						converted := <arrayStorageToMem>(value)
+						<arrayStorageToMem>(value)
 					</fromStorage>
 					<?fromCalldata>
-						converted := <allocateMemoryArray>(length)
-						<copyToMemory>(value, add(converted, 0x20), length)
+						<abiDecode>(value, <?fromDynamic>add(value, length)<!fromDynamic>calldatasize()</fromDynamic>)
 					</fromCalldata>
 				)")
 				("fromStorage", _from.dataStoredIn(DataLocation::Storage))
 				("fromCalldata", _from.dataStoredIn(DataLocation::CallData))
-				("allocateMemoryArray", _from.dataStoredIn(DataLocation::CallData) ? allocateMemoryArrayFunction(_to) : "")
-				("copyToMemory", _from.dataStoredIn(DataLocation::CallData) ? copyToMemoryFunction(true) : "")
+				("fromDynamic", _from.isDynamicallySized())
+				("abiDecode", ABIFunctions(m_evmVersion, m_revertStrings, m_functionCollector).tupleDecoder({&_to}, false))
 				("arrayStorageToMem", _from.dataStoredIn(DataLocation::Storage) ? copyArrayFromStorageToMemoryFunction(_from, _to) : "")
 				.render()
 			);
